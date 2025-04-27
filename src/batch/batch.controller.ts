bf60cdd5141a12d55751c16fd347d526bb94bc51
Nikhil Kumar <153814768+Nikhil2247@users.noreply.github.com>
2025-04-27 23:31:01 +0530

    update

--- src/batch/batch.controller.ts
+++ src/batch/batch.controller.ts
@@ -7,31 +7,31 @@ export class BatchController {
   constructor(private readonly batchService: BatchService) {}
 
   @Post()
-  @Roles('ADMISSION_OFFICER', 'PRINCIPAL','ACCOUNTANT')
+  //@Roles('ADMISSION_OFFICER', 'PRINCIPAL','ACCOUNTANT')
   create(@Body() body: { name: string; isActive?: boolean }) {
     return this.batchService.createBatch(body);
   }
 
   @Get()
-  @Roles('ADMISSION_OFFICER', 'PRINCIPAL','ACCOUNTANT')
+  //@Roles('ADMISSION_OFFICER', 'PRINCIPAL','ACCOUNTANT')
   findAll() {
     return this.batchService.findAllBatches();
   }
 
   @Get(':id')
-  @Roles('ADMISSION_OFFICER', 'PRINCIPAL','ACCOUNTANT')
+  //@Roles('ADMISSION_OFFICER', 'PRINCIPAL','ACCOUNTANT')
   findOne(@Param('id') id: string) {
     return this.batchService.findBatchById(id);
   }
 
   @Put(':id')
-  @Roles('ADMISSION_OFFICER', 'PRINCIPAL','ACCOUNTANT')
+  //@Roles('ADMISSION_OFFICER', 'PRINCIPAL','ACCOUNTANT')
   update(@Param('id') id: string, @Body() body: any) {
     return this.batchService.updateBatch(id, body);
   }
 
   @Delete(':id')
-  @Roles('ADMISSION_OFFICER', 'PRINCIPAL','ACCOUNTANT')
+  //@Roles('ADMISSION_OFFICER', 'PRINCIPAL','ACCOUNTANT')
   remove(@Param('id') id: string) {
     return this.batchService.deleteBatch(id);
   }
--- src/semester/semester.controller.ts
+++ src/semester/semester.controller.ts
@@ -7,31 +7,31 @@ export class SemesterController {
   constructor(private readonly semesterService: SemesterService) {}
 
   @Post()
- @Roles('ADMISSION_OFFICER', 'PRINCIPAL', 'TEACHER','ACCOUNTANT')
+ //@Roles('ADMISSION_OFFICER', 'PRINCIPAL', 'TEACHER','ACCOUNTANT')
   create(@Body() body: { number: number; isActive?: boolean }) {
     return this.semesterService.createSemester(body);
   }
 
   @Get()
-  @Roles('ADMISSION_OFFICER', 'PRINCIPAL', 'TEACHER','ACCOUNTANT')
+  //@Roles('ADMISSION_OFFICER', 'PRINCIPAL', 'TEACHER','ACCOUNTANT')
   getAll() {
     return this.semesterService.getAllSemesters();
   }
 
   @Get(':id')
-  @Roles('ADMISSION_OFFICER', 'PRINCIPAL', 'TEACHER','ACCOUNTANT')
+  //@Roles('ADMISSION_OFFICER', 'PRINCIPAL', 'TEACHER','ACCOUNTANT')
   getOne(@Param('id') id: string) {
     return this.semesterService.getSemesterById(id);
   }
 
   @Put(':id')
-  @Roles('ADMISSION_OFFICER', 'PRINCIPAL', 'TEACHER','ACCOUNTANT')
+  //@Roles('ADMISSION_OFFICER', 'PRINCIPAL', 'TEACHER','ACCOUNTANT')
   update(@Param('id') id: string, @Body() body: any) {
     return this.semesterService.updateSemester(id, body);
   }
 
   @Delete(':id')
-  @Roles('ADMISSION_OFFICER', 'PRINCIPAL', 'TEACHER','ACCOUNTANT')
+//  @Roles('ADMISSION_OFFICER', 'PRINCIPAL', 'TEACHER','ACCOUNTANT')
   delete(@Param('id') id: string) {
     return this.semesterService.deleteSemester(id);
   }
